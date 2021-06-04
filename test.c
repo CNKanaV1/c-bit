9b044a283935b2844ba7aa027eb9e82f3ad59a67
CNKanaV1 <mvpthresh@163.com>
2021-06-04 16:41:04 +0800

    0602上课代码

---
+++ test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//stdio - 标准输入输出
+
+//int 整型
+//int main()	//void main 过时了
+//{
+//	//库函数-打印函数-输出函数
+//	printf("hello world\n");
+//	
+//	int age = 10;
+//	int price = 50;
+//	double weight = 55.5;	//双精度
+//
+//	
+//	return 0;
+//}
+//int main()
+//{
+//		//如何计算一个类型创建的变量所占的空间的大小
+//	printf("%d\n", sizeof(char));
+//	/*printf("%d\n", 100);
+//	printf("%c\n", 100);*/
+//}
